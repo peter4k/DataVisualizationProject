60b5b9dc207eecfc773b19349d713c14ca14d883
peter4k <peter4k@msn.com>
2014-04-13 17:16:52 -0700

    Updated Scatterplot

--- javascripts/scatterplot.js
+++ javascripts/scatterplot.js
@@ -13,8 +13,8 @@ var margin = {top: 20, right: 20, bottom: 30, left: 90},
 
 var xvar = "tuition03_tf",
     yvar = "tot_rev_w_auxother_sum",
-    xname = "Tuition",
-    yname = "Total Revenue";
+    xname = "total_enrollment",
+    yname = "all_employees";
 
 // setting up x
 var xValue = function (d) {
@@ -41,14 +41,11 @@ function scatterplot(){
     width = 800 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-    console.log(width);
-    // load data from file
 
         createSPSvg();
         spform();
 
 }
-
 
 function createSPSvg() {
     svg = d3.select("#svgtd").append("svg")
@@ -93,62 +90,146 @@ function updateChart(data)
         .text(yname);
 
     // plotting the points on to the graph
-    svg.selectAll(".dot")
+    svg.selectAll("circle")
         .data(data)
-        .enter().append("circle")
-        .attr("class", "dot")
+        .enter()
+        .append("circle")
         .attr("r", 3.5)
         .attr("cx", xMap)
         .attr("cy", yMap)
-        .style("fill", "black")
+        .attr("fill", "black")
         .on("mouseover", function(d) {
-            tooltip.transition()
-                .duration(200)
-                .style("opacity", .9)
-                .style("color", "black");
-            tooltip.html(d["instname"] + "<br/> Tuition: $" + d["tuition03_tf"]
-                    + "<br/> Revenue: $" + d["tot_rev_w_auxother_sum"]
-                    + "<br/> Control: " + d["control"]
-                    + "<br/> Total Enrollment: " + d["total_enrollment"]
-                    + "<br/> Total Employees: " + d["all_employees"])
-                .style("left", (d3.event.pageX + 5) + "px")
-                .style("top", (d3.event.pageY - 28) + "px");
+            var xPosition = parseFloat(d3.select(this).attr("cx"));
+            var yPosition = parseFloat(d3.select(this).attr("cy"));
+
+            svg.append("line").attr("id", "vline")
+                .attr("x1", xPosition).attr("y1", yPosition+4)
+                .attr("x2", xPosition).attr("y2", "450")
+                .attr("style", "stroke:gray");
+
+            svg.append("line").attr("id", "hline")
+                .attr("x1", "0").attr("y1", yPosition)
+                .attr("x2", xPosition-4).attr("y2", yPosition)
+                .attr("style", "stroke:gray");
+            svg.append("text")
+                .attr("id", "mouseinfotext")
+                .attr("x", function(){
+                    if (xPosition > 300){
+                        return 100;
+                    }
+                    else{
+                        return 400;
+                    }
+                })
+                .attr("y", "50")
+                .text(d.instname)
+                .attr("font-size", "12px");
+
+
+            var text = document.getElementById("mouseinfotext");
+            var width = text.clientWidth;
+            var height = text.clientHeight;
+
+            svg.append("rect")
+                .attr("id", "mouserect")
+                .attr("x", function(){
+                    if (xPosition > 300){
+                        return 95;
+                    }
+                    else{
+                        return 395;
+                    }
+                })
+                .attr("y", "35")
+                .attr("width", width+10)
+                .attr("height", height+10)
+                .attr("fill", "rgb(220,220,220)")
+
+            d3.select("#mouseinfotext").remove();
+
+            svg.append("text")
+                .attr("id", "mouseinfotext")
+                .attr("x", function(){
+                    if (xPosition > 300){
+                        return 100;
+                    }
+                    else{
+                        return 400;
+                    }
+                })
+                .attr("y", "50")
+                .text(d.instname)
+                .attr("font-size", "12px");
+
+
         })
         .on("mouseout", function(d) {
-            tooltip.transition()
-                .duration(500)
-                .style("opacity", 0);
+            d3.select("#hline").remove();
+            d3.select("#vline").remove();
+            d3.select("#mouseinfo").remove();
+            d3.select("#mouseinfotext").remove();
+            d3.select("#mouserect").remove();
         });
 
 };
 
-function updatedSelected(){
+function transformChart(data){
 
-    svg.selectAll(".dot")
-        .data(selecteddata)
-        .enter().append("circle")
-        .attr("class", "dot")
+    xValue = function(d) {return d[xvar];},
+        yValue = function(d) {return d[yvar];},
+
+        // setting up the domain for the x and y-axis
+        xScale.domain([0, d3.max(data, xValue) + 1]);
+    yScale.domain([0, d3.max(data, yValue) + 1]);
+
+    // creating the x-axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("x", width)
+        .attr("y", -6)
+        .style("text-anchor", "end")
+        .text(xname);
+
+    // creating the y-axis
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("class", "label")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text(yname);
+
+    // plotting the points on to the graph
+    svg.selectAll("circle")
+        .data(data)
+        .transition()
         .attr("r", 3.5)
         .attr("cx", xMap)
         .attr("cy", yMap)
-        .style("fill", "red")
-        .on("mouseover", function(d) {
-            tooltip.transition()
-                .duration(200)
-                .style("opacity", .9)
-                .style("color", "black");
-            tooltip.html(d["instname"] + "<br/> Tuition: $" + d["tuition03_tf"]
-                + "<br/> Revenue: $" + d["tot_rev_w_auxother_sum"]
-                + "<br/> Control: " + d["control"]
-                + "<br/> Total Enrollment: " + d["total_enrollment"]
-                + "<br/> Total Employees: " + d["all_employees"])
-                .style("left", (d3.event.pageX + 5) + "px")
-                .style("top", (d3.event.pageY - 28) + "px");
-        })
-        .on("mouseout", function(d) {
-            tooltip.transition()
-                .duration(500)
-                .style("opacity", 0);
+        .attr("fill", function(d){
+            var color = 0;
+            schoolnames.forEach(function(n){
+                console.log(n);
+                console.log(d.instname);
+                console.log(d.instname == n);
+                if(d.instname == n){
+                    console.log("finally");
+                    color = 1;
+                }
+            })
+            if(color == 0){
+                return "black";
+            }
+            else{
+                return "red";
+            }
         });
 }
 
@@ -170,7 +251,7 @@ function chooseCategory()
         .data([])
         .exit()
         .remove();
-    updateChart(data);
+    transformChart(data);
 }
 
 function spform(){
